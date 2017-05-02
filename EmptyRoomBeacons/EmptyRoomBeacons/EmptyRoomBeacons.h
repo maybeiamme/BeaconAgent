cf1cefd091d9cbc760e8412d8c8b0958b4f31f72
Jin Hyong Park <jin@propertyguru.com.sg>
2017-05-02 14:40:00 +0800

    initial commit

---
+++ EmptyRoomBeacons/EmptyRoomBeacons/EmptyRoomBeacons.h
@@ -0,0 +1,19 @@
+//
+//  EmptyRoomBeacons.h
+//  EmptyRoomBeacons
+//
+//  Created by Jin Hyong Park on 2/5/17.
+//  Copyright © 2017 PropertyGuru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EmptyRoomBeacons.
+FOUNDATION_EXPORT double EmptyRoomBeaconsVersionNumber;
+
+//! Project version string for EmptyRoomBeacons.
+FOUNDATION_EXPORT const unsigned char EmptyRoomBeaconsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmptyRoomBeacons/PublicHeader.h>
+
+
